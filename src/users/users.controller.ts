51aea9c1b6cf0b3c10ea05b5d6c76627c6088ec2
y.zhizhnevskiy <CN7FFmHS>
2022-08-17 11:55:51 +0300

    added check and update roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,10 @@ import {CreateUserDto} from "./dto/create-user.dto";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
 import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {AddRoleDto} from "./dto/add-role.dto";
+import {BanUserDto} from "./dto/ban-user.dto";
 
 @ApiTags('Users')
 @Controller('users')
@@ -20,10 +24,34 @@ export class UsersController {
 
     @ApiOperation({summary: 'Get all users'})
     @ApiResponse({status: 200, type: [User]})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
     // guard for one end point
     @UseGuards(JwtAuthGuard)
     @Get()
     getAll(){
         return this.usersService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Set role'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    // guard with check user for one end point
+    @UseGuards(JwtAuthGuard)
+    @Post('/role')
+    addRole(@Body() roleDto: AddRoleDto){
+        return this.usersService.addRole(roleDto);
+    }
+
+    @ApiOperation({summary: 'Ban user'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    // guard with check user for one end point
+    @UseGuards(JwtAuthGuard)
+    @Post('/ban')
+    ban(@Body() banDto: BanUserDto){
+        return this.usersService.ban(banDto);
     }
 }
